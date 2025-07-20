45d390b0bdceead9bfbbb148b5069f218b7e7955
MohamedAlaaAli <muhammed.157@gmail.com>
2025-07-20 17:16:56 +0300

    remove backgorund dice calc

--- utils/metrics.py
+++ utils/metrics.py
@@ -88,7 +88,7 @@ def recall_score(preds: torch.Tensor, targets: torch.Tensor, num_classes: int) -
 @torch.no_grad
 def multiclass_dice_score(preds: torch.Tensor, targets: torch.Tensor, num_classes: int, epsilon=1e-6) -> float:
     """
-    Compute macro-average Dice score for multi-class 3D segmentation.
+    Compute macro-average Dice score for multi-class 3D segmentation, excluding the background.
 
     Args:
         preds (torch.Tensor): Logits or softmax of shape (B, C, D, H, W)
@@ -102,7 +102,7 @@ def multiclass_dice_score(preds: torch.Tensor, targets: torch.Tensor, num_classe
     preds = torch.argmax(preds, dim=1)  # shape: (B, D, H, W)
     dice_per_class = []
 
-    for cls in range(num_classes):
+    for cls in range(1, num_classes):
         pred_cls = (preds == cls).float()
         target_cls = (targets == cls).float()
 
