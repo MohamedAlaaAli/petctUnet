5456c12db6fbbb5f8aa3546f7a791a5c44c92a20
MohamedAlaaAli <muhammed.157@gmail.com>
2025-07-30 16:35:23 +0300

    reimplement data

--- utils/data.py
+++ utils/data.py
@@ -3,7 +3,6 @@ import torch
 import nibabel as nib
 from torch.utils.data import Dataset
 import numpy as np
-from models.text_model import TextEmbedder
 
 
 class PETCTDataset(Dataset):
@@ -26,19 +25,22 @@ class PETCTDataset(Dataset):
                 if not os.path.isdir(study_path):
                     continue
 
-                nii_files = [f for f in os.listdir(study_path) if f.endswith('.nii.gz')]
-
+                nii_files = [
+                    os.path.join(study_path, f, inner_f)
+                    if os.path.isdir(os.path.join(study_path, f)) else os.path.join(study_path, f)
+                    for f in os.listdir(study_path)
+                    for inner_f in ([os.listdir(os.path.join(study_path, f))[0]] if os.path.isdir(os.path.join(study_path, f)) else [f])
+                    if inner_f.endswith(('.nii', '.nii.gz'))
+                    ]                
                 pet_file = next((f for f in nii_files if 'pet' in f.lower()), None)
                 ct_file = next((f for f in nii_files if 'ctres' in f.lower()), None)
                 mask_file = next((f for f in nii_files if 'seg' in f.lower()), None)
-                txt = os.path.join(study_path, "lesion_report.txt")
 
                 if pet_file and ct_file:
                     self.samples.append({
                         'pet_path': os.path.join(study_path, pet_file),
                         'ct_path': os.path.join(study_path, ct_file),
                         'mask_path': os.path.join(study_path, mask_file) if mask_file else None,
-                        "txt":txt
                     })
 
     def __len__(self):
@@ -54,8 +56,6 @@ class PETCTDataset(Dataset):
         pet_vol = (pet_vol - pet_vol.min()) / (np.ptp(pet_vol) + 1e-6)
         ct_vol = (ct_vol - ct_vol.min()) / (np.ptp(ct_vol) + 1e-6)
 
-        # Optional: Clip extreme values if needed
-
         # Load mask (if exists)
         if sample['mask_path'] is not None:
             mask_vol = nib.load(sample['mask_path']).get_fdata().astype(np.float32)
@@ -63,15 +63,11 @@ class PETCTDataset(Dataset):
             mask_vol = np.zeros_like(ct_vol, dtype=np.float32)
 
         # Convert to tensors with shape [1, D, H, W]
-        pet_tensor = torch.from_numpy(pet_vol).unsqueeze(0)
-        ct_tensor = torch.from_numpy(ct_vol).unsqueeze(0)
-        mask_tensor = torch.from_numpy(mask_vol).unsqueeze(0)
+        pet_tensor = torch.from_numpy(pet_vol).unsqueeze(0).permute(0, 3, 1, 2)
+        ct_tensor = torch.from_numpy(ct_vol).unsqueeze(0).permute(0, 3, 1, 2)
+        mask_tensor = torch.from_numpy(mask_vol).unsqueeze(0).permute(0, 3, 1, 2)
 
-        with open(sample["txt"], "r") as s:
-            text = s.read()
-
-        
         if self.transform:
             pet_tensor, ct_tensor, mask_tensor = self.transform(pet_tensor, ct_tensor, mask_tensor)
 
-        return ct_tensor, pet_tensor, mask_tensor, text
+        return ct_tensor, pet_tensor, mask_tensor
